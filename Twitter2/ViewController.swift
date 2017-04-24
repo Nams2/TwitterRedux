a9922911d5f6a608d849269f95def37f06b696e1
Namrata Mehta <namrata@gauravs-mbp.hsd1.ca.comcast.net>
2017-04-23 21:44:26 -0700

    Initial Commit

---
+++ Twitter2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter2
+//
+//  Created by Namrata Mehta on 4/23/17.
+//  Copyright © 2017 Namrata Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
